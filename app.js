4ae06dca054d3f0507d2e51b066c7bc9e04489e2
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-03-24 22:38:35 +0500

    server running

--- app.js
+++ app.js
@@ -34,3 +34,111 @@ db.once("open", () => {
     })
 })
 
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+// create app 
+const app = express()
+
+// cors 
+app.use(cors())
+app.use(express.json())
+
+// // Routes
+const productRoute=require("./src/Routes/ProductRoute")
+
+app.use("/",productRoute)
+
+const Auth=require("./src/Routes/AuthUserRoute")
+app.use("/",Auth)
+
+// Connection 
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error"))
+db.once("open", () => {
+    console.log('Connected to MongoDB')
+    const port = process.env.PORT
+    app.listen(port, () => {
+        console.log(`Server is Listining on Port ${port}`)
+    })
+})
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+// create app 
+const app = express()
+
+// cors 
+app.use(cors())
+app.use(express.json())
+
+// // Routes
+const productRoute=require("./src/Routes/ProductRoute")
+
+app.use("/",productRoute)
+
+const Auth=require("./src/Routes/AuthUserRoute")
+app.use("/",Auth)
+
+// Connection 
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error"))
+db.once("open", () => {
+    console.log('Connected to MongoDB')
+    const port = process.env.PORT
+    app.listen(port, () => {
+        console.log(`Server is Listining on Port ${port}`)
+    })
+})
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+// create app 
+const app = express()
+
+// cors 
+app.use(cors())
+app.use(express.json())
+
+// // Routes
+const productRoute=require("./src/Routes/ProductRoute")
+
+app.use("/",productRoute)
+
+const Auth=require("./src/Routes/AuthUserRoute")
+app.use("/",Auth)
+
+// Connection 
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error"))
+db.once("open", () => {
+    console.log('Connected to MongoDB')
+    const port = process.env.PORT
+    app.listen(port, () => {
+        console.log(`Server is Listining on Port ${port}`)
+    })
+})
+
